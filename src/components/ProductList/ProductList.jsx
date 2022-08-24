68786e893a21e04291906bc2cd741d35d49238dd
Tiin24 <agustiinromero2401@gmail.com>
2022-08-24 02:43:43 -0300

    chupala ebeus

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Empanadas from '../Empanadas/Empanadas'
-import Pizza from '../Pizza/Pizza'
-import Sanguches from '../Sanguches/Sanguches'
 import Destacados from '../Destacados/Destacados'
-import Bebidas from '../Bebidas/Bebidas'
+import Empanadas from './Empanadas/Empanadas'
+import Pizza from './Pizza/Pizza'
+import Sanguches from './Sanguches/Sanguches'
+import Bebidas from './Bebidas/Bebidas'
 
 function ProductList() {
   return (
     <div>
-        <Destacados/>
-        <Pizza/>
-        <Sanguches/>
-        <Empanadas/>
-        <Bebidas/>
+      <Destacados />
+      <Pizza/>
+      <Empanadas />
+      <Sanguches/>
+      <Bebidas/>
     </div>
   )
 }
