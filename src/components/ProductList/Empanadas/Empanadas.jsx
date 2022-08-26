64b932f917e6573ddb456369ff6472d8a9197f4c
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-08-26 19:28:52 -0300

    comienzo de tienda

--- src/components/ProductList/Empanadas/Empanadas.jsx
+++ src/components/ProductList/Empanadas/Empanadas.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import data from '../../../data/data'
-import CardInfo from '../../CardInfo/CardInfo'
+import React from "react";
+import data from "../../../data/data";
+import CardInfo from "../../CardInfo/CardInfo";
+import style from "../Products.module.css";
 
 function Empanadas() {
+  const filtrado = data.filter(function (e) {
+    return e.category === "empanadas";
+  });
 
-  const filtrado = data.filter(function (e) {return e.category === 'empanadas'})
-  
-  
   return (
-    <div>
-      {
-        filtrado && (
-          <div>
-              <h1>Empanadas</h1>
-              <div>
-                {
-                  filtrado.map(function (item){
-                    return (
-                      <div key={item.id}>
-                        <CardInfo
-                          img={item.img}
-                          name={item.name}
-                          description={item.description}
-                          price={item.price}
-                          item={item}
-                          />
-                      </div>
-                    )
-                  })
-                }
-              </div>
+    <div className={style.container}>
+      {filtrado && (
+        <div>
+          <div className={style.titulo}>
+            <h1>Empanadas</h1>
           </div>
-        )
-      }
+          <div>
+            {filtrado.map(function (item) {
+              return (
+                <div key={item.id} className={style.card}>
+                  <CardInfo
+                    img={item.img}
+                    name={item.name}
+                    description={item.description}
+                    price={item.price}
+                    item={item}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Empanadas
+export default Empanadas;
