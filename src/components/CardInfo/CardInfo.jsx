4c009b336cebc39e6257e2d7d0f2bdd1b0b54d19
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-08-27 01:36:23 -0300

    revision de las cards

--- src/components/CardInfo/CardInfo.jsx
+++ src/components/CardInfo/CardInfo.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
-import {AddToCart} from "../../redux/action"
-import {useDispatch} from 'react-redux'
+import { AddToCart } from "../../redux/action";
+import { useDispatch } from "react-redux";
+import style from "./CardInfo.module.css";
 function CardInfo({ item }) {
-  const [cart,setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  function addToCart (id){
-    dispatch(AddToCart(id))
-    setCart([...cart])
-    console.log(id, `hola`)
-  } 
+  function addToCart(id) {
+    dispatch(AddToCart(id));
+    setCart([...cart]);
+  }
   return (
-    <div>
-      <div>
-        <img src={item.img} alt={item.name} width="150px" height="100px" />
-        <div>
+    <div className={style.container}>
+      <div className={style.subContainer}>
+        <div className={style.columna}>
           <h2>{item.name}</h2>
           <p>{item.description}</p>
-          <span>{"$ " + item.price}</span>
-          <button onClick={()=> addToCart(AddToCart)}>Add to Cart</button>
+          <span>{"$" + item.price}</span>
+          <button onClick={() => addToCart(AddToCart)}>
+            Agregar al carrito
+          </button>
+        </div>
+        <div className={style.imagen}>
+          <img src={item.img} alt={item.name} width='150px' height='130px' />
         </div>
       </div>
     </div>
