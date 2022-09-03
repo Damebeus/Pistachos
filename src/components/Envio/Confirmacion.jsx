23ae5f3e58688d57edf00320af99da8397c5ecc7
Tiin24 <agustiinromero2401@gmail.com>
2022-09-03 00:32:55 -0300

    se creo el copy del la orden de envio

--- src/components/Envio/Confirmacion.jsx
+++ src/components/Envio/Confirmacion.jsx
@@ -34,27 +34,46 @@ export default function Confirmacion() {
         dispatch(getOrderById(id))
       }, []);
 
+      function copiarTexto(){
+        let numOrd = document.getElementById("numero_orden").innerHTML        
+        let barrio = document.getElementById("barrio").innerHTML
+        let manzana = document.getElementById("manzana").innerHTML
+        let lote = document.getElementById('lote').innerHTML
+        let linea1 = document.getElementById("linea1").innerHTML
+        let productos = document.getElementById("productos").innerHTML
+        let producName = document.getElementById('pName').innerHTML
+        let linea2 = document.getElementById("linea2").innerHTML
+        let total = document.getElementById("total").innerHTML
+        let metododePago = document.getElementById("metodo").innerHTML
+        let linea3 = document.getElementById("linea3").innerHTML
+        let tipoDeEnvio = document.getElementById("tipoDeEnvio").innerHTML
+
+        let resultado = [numOrd,barrio,manzana,lote,linea1,productos,producName,linea2,total,metododePago,linea3,tipoDeEnvio]
+        navigator.clipboard.writeText(resultado)
+        alert('Texto Copiado ya puedes pegarlo por wsp')
+      }
+
   return (
     <div>
         {envio === null ? (<p>loading</p>): 
         (
         <div>
             <br />
-            <p>Orden numero: {orden.id}</p>
-            <p>Barrio: {envio.barrio}</p>
-            <p>Manzana: {envio.manzana}</p>
-            <p>Lote: {envio.lote}</p>
-            <p>-------------------------------------------------------</p>
-            <p>Productos</p>
+            <p id="numero_orden">Orden numero: {orden.id}</p>
+            <p id="barrio">Barrio: {envio.barrio}</p>
+            <p id="manzana">Manzana: {envio.manzana}</p>
+            <p id="lote">Lote: {envio.lote}</p>
+            <p id="linea1">-------------------------------------------------------</p>
+            <p id="productos">Productos</p>
             {
-                orden.products && orden.products.map(e => <p>{e.name}</p>)
+                orden.products && orden.products.map(e => <p id="pName">{e.name}</p>)
             }
-            <p>--------------------------------------------------------</p>
-            <p>Total: $ {totalPrice }</p>
-            <p>Metodo de Pago: {orden.metodoDePago}</p>
-            <p>-------------------------------------------------------</p>
-            <p>{orden.tipoDeEnvio}</p>
-            <button>Copiar</button>
+            <p id="linea2">--------------------------------------------------------</p>
+            <p id="total">Total: $ {totalPrice }</p>
+            <p id="metodo">Metodo de Pago: {orden.metodoDePago}</p>
+            <p id="linea3">-------------------------------------------------------</p>
+            <p id="tipoDeEnvio">{orden.tipoDeEnvio}</p>
+            <button onClick={copiarTexto}>Copiar</button>
             <p></p>
             <button>wpp</button>
         </div>)}
