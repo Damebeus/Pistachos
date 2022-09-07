d4230eedaee99ba19b5b0bd1557fa9f73013cfed
Tiin24 <agustiinromero2401@gmail.com>
2022-09-07 17:15:47 -0300

    css sin pull

--- src/components/Admin/AdminOrdern.jsx
+++ src/components/Admin/AdminOrdern.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import AdminNav from "./AdminNav/AdminNav";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllOrders, getOrderAsc, getOrderDes } from "../../redux/action";
+import styles from "./AdminOrden.module.css";
 
 function AdminOrdern() {
   const getUser = localStorage.getItem("useData");
@@ -12,17 +13,15 @@ function AdminOrdern() {
 
   const allOrdenes = useSelector((state) => state.ordenes);
 
-  
   useEffect(() => {
     dispatch(getAllOrders());
   }, [dispatch]);
-  
 
-  function handleSortAscending(e){
-    if(e.target.value === 'asc'){
-      return dispatch(getOrderAsc())
+  function handleSortAscending(e) {
+    if (e.target.value === "asc") {
+      return dispatch(getOrderAsc());
     }
-    if(e.target.value === 'desc'){
+    if (e.target.value === "desc") {
       return dispatch(getOrderDes());
     }
   }
@@ -30,11 +29,11 @@ function AdminOrdern() {
   return (
     <>
       {!getUser && !getPassword && navigate.push("/admin")}
-      <div>
+      <div className={styles.mainContent}>
         <AdminNav />
-        <h1>Ordenes Creadas</h1>
-        <div>
-          <h2>Ordenar por fechas</h2>
+        <h1 className={styles.AdminH1}>Ordenes Creadas</h1>
+        <div className={styles.ordenemiento}>
+          <h2 className={styles.AdminH2}>Ordenar por fechas</h2>
           <select onChange={(e) => handleSortAscending(e)}>
             <option>Select Order</option>
             <option value="asc">asc</option>
@@ -42,37 +41,37 @@ function AdminOrdern() {
           </select>
         </div>
         {allOrdenes && (
-          <div>
-            {allOrdenes.map(productos => {
-              const envio = {...productos.envio}
+          <div className={styles.divMain}>
+            {allOrdenes.map((productos) => {
+              const envio = { ...productos.envio };
               return (
-                <div key={productos.id}>
-                  <p>----------------------------------------------------------------------------</p>
-                  <h6>Orden Numero: {productos.id}</h6>
-                  <p>Dia del Pedido:{productos.buyDate}</p>
-                  <br/>
-                  <div>
+                <div className={styles.divChil} key={productos.id}>
+                  <div className={styles.orden}>
+                    <h6>Orden Numero: {productos.id}</h6>
+                    <p>Dia del Pedido:</p>
+                    <p>{productos.buyDate}</p>
+                  </div>
+                  <div className={styles.envio}>
                     <h4>Envio:</h4>
                     <p>Manzana: {envio.manzana}</p>
-                    <p>Lote: {envio.lote}</p>
+                    <p>Tipo de Entrega: {productos.shipping}</p>
                     <p>Barrio: {envio.barrio}</p>
+                    <p>Lote: {envio.lote}</p>
                     <p>Telefono: {envio.telefono}</p>
                     <p>Nombre Completo: {envio.nombreCompleto}</p>
                   </div>
-                  <p>Tipo de Entrega: {productos.shipping}</p>
-                  <br />
-                  <div>
+                  <div className={styles.productos}>
                     <h4>Productos:</h4>
-                    {
-                      productos.products.map((e)=> {
-                        return (
-                          <div key={e.id}>
-                            <p>{e.orderline.quantity} * {e.name}</p>
-                          </div>
-                        )
-                      })
-                    }
-                    <p>Total: {productos.totalPrice}</p>
+                    {productos.products.map((e) => {
+                      return (
+                        <div key={e.id}>
+                          <p>
+                            {e.orderline.quantity} * {e.name}
+                          </p>
+                        </div>
+                      );
+                    })}
+                    <p>Total: ${productos.totalPrice}</p>
                   </div>
                 </div>
               );
